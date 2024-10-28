10c995ec60ce11b69f691842654c4bf777b7eefd
udit-gulati <uditgulati6@gmail.com>
2024-10-28 18:35:16 +0530

    add structs for reading list of transactions from data/ json file

--- mockserver/data_reader.go
+++ mockserver/data_reader.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/btcsuite/btcd/btcjson"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 )
 
@@ -28,8 +29,37 @@ type BlockHeader struct {
 	NextBlock chainhash.Hash `json:"nextblockhash"`
 }
 
+type Transaction struct {
+	TxId          chainhash.Hash `json:"txid"`
+	Hash          chainhash.Hash `json:"hash"`
+	Version       int32          `json:"version"`
+	Size          int32          `json:"size"`
+	VSize         int32          `json:"vsize"`
+	Weight        int32          `json:"weight"`
+	LockTime      uint32         `json:"locktime"`
+	VIn           []Vin          `json:"vin"`
+	VOut          []btcjson.Vout `json:"vout"`
+	Hex           string         `json:"hex"`
+	BlockHash     chainhash.Hash `json:"blockhash"`
+	Confirmations uint32         `json:"confirmations"`
+	Time          uint32         `json:"time"`
+	BlockTime     uint32         `json:"blocktime"`
+}
+
+// copied from btcjson/chainsvrresults.go and
+// modified to be compatible with bitcoin v1
+type Vin struct {
+	Coinbase string `json:"coinbase"`
+	// Txid      string     `json:"txid"`
+	// Vout      uint32     `json:"vout"`
+	// ScriptSig *ScriptSig `json:"scriptSig"`
+	Sequence uint32 `json:"sequence"`
+	// Witness  []string `json:"txinwitness"`
+}
+
 type DataContent struct {
 	BlockHeaders []BlockHeader `json:"block_headers"`
+	Transactions []Transaction `json:"transactions"`
 }
 
 type DataStore struct {
